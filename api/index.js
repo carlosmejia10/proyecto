b1f0f1e50fecc20a57f05c80a9d6dbbdaee7868b
carlosmejia10 <mejiaesteban10@gmail.com>
2023-05-06 19:10:20 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -5,16 +5,16 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
